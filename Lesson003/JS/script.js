8ab7cbd6a54b55dc49d9db3d1e0bafcf2fcbdd70
RomanDRV55555 <didenkor@gmail.com>
2022-12-17 22:54:03 +0100

    Lesson 003

--- Lesson003/JS/script.js
+++ Lesson003/JS/script.js
@@ -58,6 +58,71 @@ function countLetter(letterToFind, word){
     return result;
 }
 
+function converter(strSum = ''){
+
+    const USD_KURS = (function (){
+        let currKurs = -1;
+        let ajax=new XMLHttpRequest();
+        ajax.open('GET', 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json', false);
+        ajax.send(null);
+        if(ajax.status == 200) {
+            jsonResp = JSON.parse(ajax.response);
+            for(let i=0; i<jsonResp.length;i++){
+                if (jsonResp[i].cc == 'USD'){
+                    currKurs = jsonResp[i].rate;
+                }
+            }
+        }    
+    
+        return currKurs;
+    }());
+    
+;
+
+    if (strSum.indexOf("$") >=0 ){
+        return ((Number.parseInt(strSum.replace("$", "")) * USD_KURS).toFixed(2)) + " UAH";
+    } else if (strSum.indexOf("UAH") >= 0 || /uah/i.test(strSum)) { 
+        return ((Number.parseInt(strSum.replace("UAH", "")) / USD_KURS).toFixed(2)) + " $";
+    } else { 
+        // це не валютна сума 
+        console.error(`${strSum} є невірнім параметром!`);
+        return `невідомо чому, бо ${strSum} не є валютною сумою. І це є помилка.`; 
+
+    }
+    
+}
+
+function getRandomPassword(numberOfSymbols = 8){
+    result = "";
+
+    for (let i = 0; i<numberOfSymbols; i++){
+        result += getRandomNumber(0, 9);
+    }
+
+    return result;
+}
+
+function deleteLetters(symbolForDelete = "", wordForProc = ""){
+
+    // result = "";
+    // for (let i=0; i < wordForProc.length; i++){
+    //     if (!wordForProc[i] === symbolForDelete){
+    //         result += wordForProc[i];
+    //     }
+    // }
+
+    // return result;
+
+    result = wordForDelSymbol;
+    debugger
+    while(new RegExp(symbolForDelete, "i").test(result)){
+        result.replace(symbolForDelete,"")
+    }
+
+    return result;
+} 
+
+
 const intNumber = 123424;
 document.writeln(`Функція №1. Найбільша цифра в числі ${intNumber} це ${getMaxDigit(intNumber)} <br>`);
 const num = 2, pow = 6;
@@ -72,6 +137,10 @@ document.writeln(`Функція №5. Випадкове ціле число в
 const letter = "i";
 const word = "classification";
 document.writeln(`Функція №6. Кількість букв '${letter}' в слові '${word}' дорівнює ${countLetter(letter, word)} <br>`);
-
-
-
+const strSum = "100$";
+document.writeln(`Функція №7. '${strSum}' дорівнює ${converter(strSum)} <br>`);
+const  numberOfSymbols = 11;
+document.writeln(`Функція №8. Випадковий пароль з '${numberOfSymbols}' символів буде ${getRandomPassword(numberOfSymbols)} <br>`);
+const  wordForDelSymbol = "expression";
+const curSymbolForDel = "s";
+document.writeln(`Функція №9. Слово '${wordForDelSymbol}' після видалення символа ${curSymbolForDel} буде вигладати так ${deleteLetters(curSymbolForDel, wordForDelSymbol)} <br>`);
