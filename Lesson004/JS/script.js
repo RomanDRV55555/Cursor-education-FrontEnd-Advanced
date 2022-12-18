eedeb2da420b2e7bd98e0a0e53813e45a819c991
RomanDRV55555 <didenkor@gmail.com>
2022-12-18 19:37:31 +0100

    Lesson004 first commit

---
+++ Lesson004/JS/script.js
@@ -0,0 +1,26 @@
+const students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"]; 
+const themes = ["Диференційне рівняння", "Теорія автоматів", "Алгоритми і структури даних"]; 
+const marks = [4, 5, 5, 3, 4, 5]; 
+
+// створює пари з масиву студентів 
+function getPairs(students){
+    const arrMaleNames = ["Олександр", "Ігор", "Олексій"];
+    const arrFemaleNames = ["Олена", "Іра", "Світлана"];
+
+    
+
+
+}
+
+const pairs = getPairs();
+
+// розподялє теми по парах студентів
+function getPairsThemes(pairs, themes){}
+
+const arrPairsThemes = getPairsThemes(pairs, themes);
+
+// розподіляє оцінки по студентах
+function getStudentsMarks(students, marks){}
+
+// виставляє парам студентів випадкові оцинки за темах
+function getMarksPairs(arrPairsThemes){}
