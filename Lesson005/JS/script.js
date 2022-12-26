7fef2fa7a3f6c09f0995e88913ba5463d7f67cad
RomanDRV55555 <didenkor@gmail.com>
2022-12-26 16:04:34 +0100

    Lesson 005. Tasks till 8

--- Lesson005/JS/script.js
+++ Lesson005/JS/script.js
@@ -1,26 +1,91 @@
-function getRandomArray(length = 0, min = Number.MIN_VALUE, max = Number.MAX_VALUE){
-// forEach та map не процюють з пустим масивом, тому використовується from 
-// першим параметром передається пустий мисив і для кожного єлемента виконується callback з другого параметра
-    return Array.from(Array(length), () => Math.round(Math.random() * (max - min) + min));    
+function getRandomArray(length = 0, min = Number.MIN_VALUE, max = Number.MAX_VALUE) {
+    // forEach та map не процюють з пустим масивом, тому використовується from 
+    // першим параметром передається пустий мисив і для кожного єлемента виконується callback з другого параметра
+    return Array.from(Array(length), () => Math.round(Math.random() * (max - min) + min));
 }
 
-function getModa(...numbers){
+function getModa(...numbers) {
     let maxCount = 0;
     let resultElement;
-    
+
     // перший параметр - елемент, дргий параметр - індекс, третій параметр - сам масив
-    numbers.forEach((curElement,i,numbers)=>  {
-        
-        let curElCount = numbers.filter((elForFiler) => elForFiler === curElement).length;
-        if (curElCount > maxCount ){
-            maxCount = curElCount;
-            resultElement = curElement;
+    numbers.forEach((curElement, i, numbers) => {
+
+        // тут можна зробити через filter, але так мені зрозуміліше 
+        // через filter в getMedian
+        if (Number.isInteger(curElement)) {
+
+            let curElCount = numbers.filter((elForFiler) => elForFiler === curElement).length;
+            if (curElCount > maxCount) {
+                maxCount = curElCount;
+                resultElement = curElement;
+            }
         }
-    } )
+    })
 
     return resultElement;
 }
 
+function getAverage(...numbers){
 
-console.log(getRandomArray(5,6,9));
-console.log(getModa(5,5,4,2,2,6,7,7,7));
+    return numbers.reduce((sum, curElemet) => {
+        if (Number.isInteger(curElemet)){
+            sum += curElemet;
+        }
+        return sum;
+    })/numbers.length;
+}
+
+function getMedian(...numbers){
+    if (numbers.length === 0) {
+        return null;
+    }
+
+    const numbersSorted = numbers.filter((curElement) => Number.isInteger(curElement)).sort((a, b) => a - b);
+    const mid = Math.floor(numbersSorted.length / 2);
+
+    if (!((numbers.length % 2) === 0)) {
+        return numbersSorted[mid];
+    } else {
+        return ((numbersSorted[mid-1] + numbersSorted[mid])/2).toFixed(2);
+    }
+}
+
+function filterEvenNumbers(...numbers){
+    return numbers.filter((curElement) => (!((curElement%2)===0)));
+}
+
+function countPositiveNumbers(...numbers){
+    return numbers.filter((curElemen) => (curElemen > 0)).length;
+}
+
+function getDividedByFive(...numbers){
+    return numbers.filter((curElement) => ((curElement%5)===0));
+}
+
+function replaceBadWords(string, arrBadWords = ['fuck', 'shit']){
+
+    
+
+}
+
+console.log(getRandomArray(5, 6, 9));
+
+console.log(getModa(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
+
+console.log(getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
+
+console.log(getMedian(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2)); 
+console.log(getMedian(1, 2, 3, 4));
+console.log(getMedian(1, 2, 3, 4, 5));
+console.log(getMedian());
+
+console.log(filterEvenNumbers(1, 2, 3, 4, 5, 6));
+
+console.log(countPositiveNumbers(1, -2, 3, -4, -5, 6));
+
+console.log(getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
+
+console.log(replaceBadWords("Are you fucking kidding?"));
+console.log(replaceBadWords("Holy shit!"));
+console.log(replaceBadWords("It's bullshit!"));
