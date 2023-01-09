3f60c222cd309d38a14870865e634597e45159f3
RomanDRV55555 <didenkor@gmail.com>
2023-01-09 17:22:57 +0100

    Lesson 008: Advanced

--- Lesson008/JS/script.js
+++ Lesson008/JS/script.js
@@ -13,7 +13,6 @@ class Student {
 
     /* Створіть метод this.getInfo() -> "Студент 1го курсу Вищої Школи Психотерапії м.Одеса, Остап Родоманський Бендер", 
     метод повертає сукупну інформацію про курс, учбовий заклад та імені студента.  */
-
     getInfo() {
         return `Студент ${this.course} курсу ${this.university}, ${this.fullName} `;
     }
@@ -22,10 +21,8 @@ class Student {
     get marks() {
         return this._active ? this._marks : null; 
     }
-
     /*     Створіть сеттер оцінок this.marks = 5, який дозволяє поставити оцінку студенту. 
         Після того, як оцінка поставлена, геттер повинен повернути масив this.marks -> [5, 4, 4, 5, 5] */
-
     set marks(newMark){
         if (this._active) { 
             this._marks.push(newMark);
@@ -44,7 +41,6 @@ class Student {
     get active(){
         return this._active;
     }
-
     set active(curAct){
         this._active = curAct;
     } 
@@ -60,7 +56,7 @@ class Student {
     }
 }
 
-let newStudent = new Student('НАУ', 1, 'Петренко Петро Петрович', []);
+let newStudent = new Student('НАУ', 1, 'Петренко Петро Петрович');
 console.log(newStudent.getInfo());
 
 console.log(newStudent.marks);
@@ -70,6 +66,36 @@ newStudent.dismiss();
 console.log(newStudent.marks);
 newStudent.recover();
 console.log(newStudent.marks);
+
+/* Створіть новий клас BudgetStudent, який повністю наслідує клас Student */
+
+class BudgetStudent extends Student {
+
+    static sumBudgetScholarship = 1400;
+
+    constructor(university, course, fullName){
+        super(university, course, fullName);
+        this.sumScholarship = BudgetStudent.sumBudgetScholarship;   
+/*  Метод отримання стипендії автоматично викликається кожні 30 секунд післе створення об'єкту. 
+Підказка: викликайте його в constructor */
+        setInterval(() => this.getScholarship(), 30000);   
+    }
+
+
+/*     Бюджетний студент може отримати стипендію за допомогою методу this.getScholarship. 
+Отримання стипендії супроводжується виведенням інформації в консоль: Ви отримали 1400 грн. стипендії  */
+    getScholarship(){
+/*     Якщо студента виключено, він не отримує стипендію */        
+    if (this.active){
+/*  Студент отримує стипендію тільки в тому випадку, якщо середній бал у нього вище або дорівнює 4.0  */
+        if (this.getAverageMark() > 4)
+            console.log(`Ви отримали ${this.sumScholarship} грн. стипендії`);
+        }
+    } 
+}
+
+newBudgetStudent = new BudgetStudent('КПІ',2,'Степаненко Степан Степанович');
+
 
 
 
